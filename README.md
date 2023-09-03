1326c26aa5fe9a54e43b2479b6f5ffbe17fdcd09
surajbhandvale <Bhandvale2022@#>
2023-09-03 10:22:08 +0530

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java-DSA
 Sample code for Data Structures and algorithms using Java
+I will add more code day by day in thius repository
--- src/com/examples/Main.java
+++ src/com/examples/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         
         System.out.printf("Hello and welcome!");
-
+        System.out.println("this is my second commit");
     }
 }
